971d501506024b925bfcd617e03c9007e3fcd0f4
Denis Burenkov <denbyrenk@gmail.com>
2023-07-26 10:31:02 +0300

    Исправленный отступы!

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -17,47 +17,47 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-            updateClicLabel()
-            historyTextView.text = historyText
-            historyTextView.isEditable = false
-        }
-        
-        func updateClicLabel() {
-            clicLabel.text = "\(count)"
-        }
-        
-        func addToHistory(_ event: String) {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
-            let dateString = dateFormatter.string(from: Date())
-            historyText += "[\(dateString)]: \(event)\n"
-            historyTextView.text = historyText
-        }
-        
-        @IBAction func redPlusOneButton(_ sender: UIButton) {
-            if count < Int.max {
-                count += 1
-                updateClicLabel()
-                addToHistory("Значение изменено на +1")
-            }
-        }
-        
-        @IBAction func blueMinisOneButton(_ sender: UIButton) {
-            if count > 0 {
-                count -= 1
-                updateClicLabel()
-                addToHistory("Значение изменено на -1")
-            } else {
-                addToHistory("Внимание! Попытка уменьшить значение счётчика ниже 0!")
-            }
-        }
-        
-        @IBAction func reternButton(_ sender: UIButton) {
-            count = 0
-            updateClicLabel()
-            addToHistory("Значение сброшено")
-        }
-    
+        super.viewDidLoad()
+        updateClicLabel()
+        historyTextView.text = historyText
+        historyTextView.isEditable = false
     }
+    
+    func updateClicLabel() {
+        clicLabel.text = "\(count)"
+    }
+    
+    func addToHistory(_ event: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateString = dateFormatter.string(from: Date())
+        historyText += "[\(dateString)]: \(event)\n"
+        historyTextView.text = historyText
+    }
+    
+    @IBAction func redPlusOneButton(_ sender: UIButton) {
+        if count < Int.max {
+            count += 1
+            updateClicLabel()
+            addToHistory("Значение изменено на +1")
+        }
+    }
+    
+    @IBAction func blueMinisOneButton(_ sender: UIButton) {
+        if count > 0 {
+            count -= 1
+            updateClicLabel()
+            addToHistory("Значение изменено на -1")
+        } else {
+            addToHistory("Внимание! Попытка уменьшить значение счётчика ниже 0!")
+        }
+    }
+    
+    @IBAction func reternButton(_ sender: UIButton) {
+        count = 0
+        updateClicLabel()
+        addToHistory("Значение сброшено")
+    }
+    
+}
 
